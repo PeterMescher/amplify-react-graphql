6c18ea1533339d8651cf4f78b03e5f8d9b553f48
PeterMescher <peter@mescher.net>
2023-11-01 10:35:25 -0400

    Added graphql and storage v2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import './App.css';
 import "@aws-amplify/ui-react/styles.css";
 import { API, Storage } from "aws-amplify";
 import {
+  withAuthenticator,
   Button,
-  Flex,
   Heading,
-  Image,
+  View,
+  Flex,
   Text,
   TextField,
-  View,
-  withAuthenticator,
+  Image,
 } from "@aws-amplify/ui-react";
 import { listNotes } from "./graphql/queries";
 import {
   createNote as createNoteMutation,
-  deleteNote as deleteNoteMutation,
+  deleteNote as deleteNoteMustation,
 } from "./graphql/mutations";
 
 const App = ({ signOut }) => {
@@ -25,6 +25,7 @@ const App = ({ signOut }) => {
     fetchNotes();
   }, []);
 
+  // uses API class to send query to GraphQL API & retrive list of notes
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listNotes });
     const notesFromAPI = apiData.data.listNotes.items;
@@ -40,9 +41,11 @@ const App = ({ signOut }) => {
     setNotes(notesFromAPI);
   }
 
+  // uses API class to send mutation to GraphQL API, difference is in passing variables needed for a mutation to create a new note with the form data
   async function createNote(event) {
     event.preventDefault();
     const form = new FormData(event.target);
+    const image = form.get("image");
     const data = {
       name: form.get("name"),
       description: form.get("description"),
@@ -57,12 +60,13 @@ const App = ({ signOut }) => {
     event.target.reset();
   }
 
-  async function deleteNote({ id }) {
+  // like create, this sends GraphQL a mutation w/ variables but to delete a note
+  async function deleteNote({ id, name }) {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
     await Storage.remove(name);
     await API.graphql({
-      query: deleteNoteMutation,
+      query: deleteNoteMustation,
       variables: { input: { id } },
     });
   }
@@ -87,6 +91,12 @@ const App = ({ signOut }) => {
             labelHidden
             variation="quiet"
             required
+          />
+          <View
+            name="image"
+            as="input"
+            type="file"
+            style={{ alignSelf: "end" }}
           />
           <Button type="submit" variation="primary">
             Create Note
@@ -113,22 +123,15 @@ const App = ({ signOut }) => {
                 style={{ width: 400 }}
               />
             )}
-           <Button variation="link" onClick={() => deleteNote(note)}>
-              Delete note
+            <Button variation="link" onClick={() => deleteNote(note)}>
+              DeleteNote
             </Button>
           </Flex>
         ))}
       </View>
-      <Button onClick={signOut}>Sign Out</Button>
+      <Button onClick={signOut}>SignOut</Button>
     </View>
-  <View
-    name="image"
-    as="input"
-    type="file"
-    style={{ alignSelf: "end" }}
-  />
   );
 };
 
 export default withAuthenticator(App);
-
